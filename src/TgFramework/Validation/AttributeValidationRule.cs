f042ee904d9d37230523b1432722303317466266
geodanila <geo.danila89@gmail.com>
2015-02-03 21:51:23 +0200

    Sorry Traian

---
+++ src/TgFramework/Validation/AttributeValidationRule.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Windows.Controls;
+using ValidationResult = System.Windows.Controls.ValidationResult;
+
+namespace TgFramework.Validation
+{
+    public class AttributeValidationRule : ValidationRule
+    {
+        private readonly ValidationAttribute _attribute;
+        private readonly object _dataContext;
+
+        public AttributeValidationRule(ValidationAttribute attribute, object dataContext)
+        {
+            _attribute = attribute;
+            _dataContext = dataContext;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var result = _attribute.GetValidationResult(value, new ValidationContext(_dataContext));
+            if (result == System.ComponentModel.DataAnnotations.ValidationResult.Success)
+            {
+                return ValidationResult.ValidResult;
+            }
+
+            return new ValidationResult(false, result.ErrorMessage);
+        }
+    }
+}
